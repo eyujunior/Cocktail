8552cb8d74d1d70c2d56ce9bb9a6517a2d414179
eyujunior <eyujunior052@gmail.com>
2022-05-25 17:39:09 -0400

    setup and clean up

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['poppins', 'system-ui'],
+      title: ['Abril Fatface', 'Georgia'],
+    },
+    backgroundImage: {
+      hero: "url('./assets/img/bg.jpg')",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
